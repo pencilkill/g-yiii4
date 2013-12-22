6caf6e399815e181e92ff6513cb0c6938d2c382f
Sam <sam@ozchamp.net>
2013-12-23 04:38:41 +0800

    update ajaxuploadhandler
    update ajaxuploadhandler

--- protected/extensions/ajaxupload/assets/handler.js
+++ protected/extensions/ajaxupload/assets/handler.js
@@ -1,83 +1,91 @@
 /**
+ * ajaxUploadHandler
+ * 
+ * This is a helper plugin based on jQuery, AjaxUpload
+ * Notice: ajaxUploadHandler selector is a field element which used for keeping the AjaxUpload return value while that AjaxUpload selector is a button element for upload file 
+ * 
  * 2013-07-20
  * @author Sam@ozchamp.net
  * @base jQuery
  * @version 1.0
+ * 
  */
 (function($){  
 
-	$.fn.ajaxUploadHandler=function(options) {
-		
-        return this.each(function() {       	
-        	var defaults = {
-        			action: 'upload.php',
-        			data:{},
-        			name: 'userfile',
-        			autoSubmit: true,
-        			responseType: 'json',
-        			onChange: function(file, extension){
-        				imageChange(file, extension);
-        			},
-        			onSubmit: function(file, extension){
-        				imageSubmit(file, extension);
-        			},
-        			onComplete: function(file, json){ 
-        				imageComplete(file, json);
-        			}
-        	}
+	$.fn.ajaxUploadHandler=function(options) {	
+		function onChange(file, extension) {
+    		// check extension
+    		return true;
+    	}
+    	
+    	function onSubmit(file, extension) {
+    		var btn = $(this._button);	// or just use $(this)
+    		
+    		btn.after('<img src="' + _options.baseUrl + '/images/loading.gif" class="ajaxUploadLoading" style="padding-left: 5px;" />');
+    		btn.attr('disabled', true);
+    		
+    		return true;
+    	}
+    	
+    	function onComplete(file, json) {
+    		// 'this' refers to the element's Ajaxupload, not the element
+    		var btn = $(this._button);
+    		var preview = $(btn.attr('previewId'));
+    		var hidden = $(btn.attr('hiddenId'));
+    		
+    		btn.attr('disabled', false);
+    		
+    		if (json['success']) {
+    			//alert(json['success']);
+    			if(preview){
+	    			var tagName = preview.prop('tagName').toString().toUpperCase();
+	    			
+	    			if(tagName=='IMG'){
+	    				preview.attr('src', json.src);
+	    			}else if(tagName=='A'){
+	    				preview.attr('href', json.file);
+	    			}
+    			}
+    			
+    			if(hidden){
+    				hidden.attr('value', json.file);
+    			}
+    		}
+    		if (json['error']) {
+    			alert(json['error']);
+    		}
+    		$('.ajaxUploadLoading').remove();
+    	}
+    	
+		var defaults = {
+				action: undefined,
+				data:{},
+				name: 'userfile',	// compatiable with Ajaxupload, be carefully to change this default setting
+				autoSubmit: true,
+				responseType: 'json',
+				onChange: onChange,
+				onSubmit: onSubmit,
+				onComplete: onComplete
+		};
+    	
+    	var _options = $.extend({}, defaults, options);
+    	
+        return this.each(function() {
+        	var btnId = _options.btnId ? _options.splice('btnId') : '#' + this.id + '_upload';	// remove after getting
+        	var previewId = _options.previewId ? _options_options.splice('previewId') : (this.previewId ? this.previewId : '#' + this.id + '_preview');
+        	var hiddenId = '#' + this.id;
         	
-        	var _options = $.extend({}, defaults, options);
+        	$(btnId).attr({
+        		'hiddenId':hiddenId,
+        		'previewId':previewId
+        	});
 
-        	var id = $(this).attr('id');
-			
-        	if(id){
-				// hidden input element id, the data submitted finally
-				var hiddenId='#' + id;
-				// preview image element id
-				var imageId = hiddenId + '_preview';
-				// the element created by ajaxupload which is float to support ajaxupload
-				var fileId = hiddenId+'_upload';
-				
-				$(this).data('settings', _options);
-				
-				imageUploader(fileId, _options);
-				
-			}
-        	
-        	function imageUploader(fileId, _options) {
-        		new AjaxUpload(fileId, _options);
+        	if(_options.action){
+        		new AjaxUpload(btnId, _options);
+        	}else{
+        		throw 'ajaUploadHanler: action for AjaxUpload is required!';
         	}
-        	
-        	function imageChange(file, extension) {
-        		// check extension
-        		return true;
-        	}
-        	
-        	function imageSubmit(file, extension) {
-        		$(fileId).after('<img src="' + _options.baseUrl + '/images/loading.gif" class="ajaxUploadLoading" style="padding-left: 5px;" />');
-        		$(fileId).attr('disabled', true);
-        		return true;
-        	}
-        	
-        	function imageComplete(file, json) {
-        		$(imageId).attr('disabled', false);
-        		if (json['success']) {
-        			//alert(json['success']);
-        			var tagName = $(imageId).prop('tagName').toUpperCase();
-        			
-        			if(tagName=='IMG'){
-        				$(imageId).attr('src', json.src);
-        			}else if(tagName=='A'){
-        				$(imageId).attr('href', json.file);
-        			}
-        			
-        			$(hiddenId).attr('value', json.file);
-        		}
-        		if (json['error']) {
-        			alert(json['error']);
-        		}
-        		$('.ajaxUploadLoading').remove();
-        	}
+        	//$(this).data('ajaxUploadHandler', new AjaxUpload(this.id + '_upload', _options));	// debug       	
         });
     };  
     
