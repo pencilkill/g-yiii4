84234c71426dfbdf14aed187fab2d50a0fe99c8a
Sam <sam@ozchamp.net>
2014-03-15 09:53:47 +0800

    HCInput fgetcsv update

--- protected/helpers/HCInput.php
+++ protected/helpers/HCInput.php
@@ -6,37 +6,73 @@
  */
 class HCInput {
 	/**
-	 * Compatible with Chinese
+	 * fgetcsv, Compatible Chinese
 	 *
-	 * @param $fp
-	 * @param $l
-	 * @param $d
-	 * @param $e
+	 * @param string $filename
+	 * @param string $locale, setlocale for csv file
+	 * @param string $to, mb_convert_encoding to charset for field
+	 * @param string $from, mb_convert_encoding from charset for field
+	 * @return multitype:multitype:string
 	 */
-	public static function fgetcsv(&$fp, $l = null, $d = ',', $e = '"') {
-		$d = preg_quote($d);
-		$e = preg_quote($e);
+	public static function fgetcsv($filename, $locale = 'en_US.UTF-8', $to = 'UTF-8', $from = 'UTF-8,BIG5,GBK')
+	{
+		setlocale(LC_ALL, 'en_US.UTF-8');
 
-		$_line = "";
-		$eof=false;
-		while($eof != true){
-			$_line .= (empty($l) ? fgets($fp) : fgets($fp, $l));
-			$itemcnt = preg_match_all('/' . $e . '/', $_line, $dummy);
-			if($itemcnt % 2 == 0){
-				$eof = true;
+		$result=array();
+
+		$index = 1;
+
+		if(($fp = fopen($filename, 'r')) !== false)
+		{
+			while(($row = self::_fgetcsv($fp)) !== false)
+			{
+				$_row=array();
+
+				for ($i = 0, $n = sizeof($row); $i < $n; $i++)
+				{
+					if($index === 1)
+					{
+						$header[] = $row[$i];
+					}
+					else
+					{
+						foreach ($header as $k=>$v)
+						{
+							if($k == $i)
+							{
+
+								$row[$i]=  mb_convert_encoding($row[$i],'UTF-8','UTF-8,GBK,BIG5');	// origin encoding : GBK
+								//$row[$i]=  mb_convert_encoding($row[$i],'UTF-8','UTF-8,BIG5,GBK');	// origin encoding : BIG5
+
+								/*
+								 * Column callback
+								*/
+
+								//
+								$_row[$v]=$row[$i];
+					 	}
+						}
+
+						/*
+						 * Row callback
+						*/
+
+					}
+
+				}
+
+				if(!empty($_row))
+				{
+					$result[] = $_row;
+				}
+
+				$index++;
+
 			}
+			fclose($fp);
 		}
 
-		$_csv_line = preg_replace('/(?: |[ ])?$/', $d, trim($_line));
-		$_csv_pattern = '/(' . $e . '[^' . $e . ']*(?:' . $e . $e . '[^' . $e . ']*)*' . $e . '|[^' . $d . ']*)' . $d . '/';
-		preg_match_all($_csv_pattern, $_csv_line, $_csv_matches);
-		$_csv_data = $_csv_matches[1];
-		for ($_csv_i = 0; $_csv_i < count($_csv_data); $_csv_i++) {
-			$_csv_data[$_csv_i] = preg_replace('/^' . $e . '(.*)' . $e . '$/s', '$1' , $_csv_data[$_csv_i]);
-			$_csv_data[$_csv_i] = str_replace($e . $e, $e, $_csv_data[$_csv_i]);
-		}
-
-		return empty ($_line) ? false : $_csv_data;
+		return $result;
 	}
 }
 ?>
