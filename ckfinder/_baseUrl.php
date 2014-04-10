3d4c863caa82529c5c49e09f7fee2950650ead14
Sam <sam@ozchamp.net>
2014-04-10 19:23:51 +0800

    .

--- ckfinder/_baseUrl.php
+++ ckfinder/_baseUrl.php
@@ -4,26 +4,28 @@ class _baseUrl{
 	const DEFAULT_PROTOCOL = 'http';
 
 	public static function dir($baseDir = NULL){
-		if($baseDir === false){
-			return '';
-		}else{
-			return $baseDir = rtrim(strtr(realpath($baseDir ? $baseDir : __DIR__ . '/' . self::BASE_DIR), array('\\'=>'/','\/'=>'/')), '/') . '/';
-		}
+		return $baseDir === false ? '' : rtrim(strtr(realpath($baseDir ? $baseDir : __DIR__ . '/' . self::BASE_DIR), array('\\'=>'/','\/'=>'/')), '/') . '/';
 	}
 
 	public static function url($baseDir = NULL, $protocol = NULL){
-		if($baseDir === false){
-			return '';
-		}else{
-			$baseDir = self::dir($baseDir);
+		$baseUrl = '';
 
-			$baseUrl = strtr('//' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'], array(strtr($_SERVER['SCRIPT_FILENAME'], array($baseDir=>''))=>''));;
+		$baseDir = self::dir($baseDir);
 
-			if($protocol === NULL) $protocol = self::DEFAULT_PROTOCOL;
+		if($baseDir){
+			if($protocol === NULL){
+				$protocol = self::DEFAULT_PROTOCOL;
+			}
 
-			$colon = $protocol ? ':' : '';
+			if($protocol){
+				$protocol .= ':';
+			}
 
-			return $protocol . $colon . $baseUrl;
+			$url = strtr('//' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'], array(strtr($_SERVER['SCRIPT_FILENAME'], array($baseDir=>''))=>''));;
+
+			$baseUrl = $protocol . $url;
 		}
+
+		return $baseUrl;
 	}
 }
