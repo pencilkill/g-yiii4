0b0e85d298f7ae40fdea9a8a32a379cde9c46d3c
pencilkill <sam@ozchamp.net>
2014-04-13 22:19:48 +0800

    giix swfupload template update

--- protected/extensions/giix-core/giixCrud/templates/_ozman/_swfupload.php
+++ protected/extensions/giix-core/giixCrud/templates/_ozman/_swfupload.php
@@ -6,32 +6,18 @@ Yii::t('app', 'Confirm Gallery Image Delete?');
 Yii::t('app', 'Gallery Image Delete');
  * ************** end of swfupload extension translations *********
 */
-$modelClass = CHtml::modelName($gallery);
-$languages = CHtml::listData($this->languages, '<?php echo GiixModelCode::I18N_LANGUAGE_COLUMN_NAME?>', 'code');
 
 $this->widget('frontend.extensions.swfupload.CSwfUpload', array(
-		// defined in init()
-		//'jsHandlerUrl'=>$baseUrl.'/handlers.js',
-		//
-		'galleries' => $galleries,
-		// This will be merge to config's post_params, see init()
-		'postParams' => array(
-			'modelClass' => $modelClass,
-			'languages' => $languages,
-			// thumbnail resize
-			//'resize' => array(),
+		'photos' => $photos,
+		'model' => CHtml::modelName($photo),
+		//'attribute' => 'pic',
+		'attributes' => array(
+			array(
+				//'attribute' => 'product_id',
+			),
 		),
-		//
-		//This will be merge to config's custom_setting, see init()
-		/*
-		'customSettings' => array(
-			// Notic that '?' is required if thumb_url has no query string
-			'thumb_url'=>CHtml::normalizeUrl(Yii::app()->createUrl('conroller/action')),
-		),
-		*/
-		//This will be merge to config, see init()
 		'config' => array(
-			'button_text'=>'<span class="button">'.Yii::t('app', 'Select A Image').' [400 * 300]</span>',
+			'button_text' => '<span class="button">'.Yii::t('app', 'Select A Image').' [400 * 300]</span>',
 		),
     )
 );
