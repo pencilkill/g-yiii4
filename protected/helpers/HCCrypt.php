54c3dc1f26c7a4f3cc572933e92713a4e803baf7
pencilkill <sam@ozchamp.net>
2014-04-27 03:50:32 +0800

    .

---
+++ protected/helpers/HCCrypt.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * 2014-04-27
+ *
+ * @author Sam <mail.song.de.qiang@gmail.com>
+ *
+ */
+
+class HCCrypt{
+	const KEY = __CLASS__;
+
+	public static function encrypt($data, $key){
+		$key = md5($key);
+		$x = 0;
+		$len = strlen($data);
+		$l = strlen($key);
+		$char = '';
+		for($i = 0; $i < $len; $i++){
+			if($x == $l){
+				$x = 0;
+			}
+			$char .= $key{$x};
+			$x++;
+		}
+		$str = '';
+		for($i = 0; $i < $len; $i++){
+			$str .= chr(ord($data{$i}) + (ord($char{$i})) % 256);
+		}
+
+		return base64_encode($str);
+	}
+
+	public static function decrypt($data, $key) {
+		$key = md5($key);
+		$x = 0;
+		$data = base64_decode($data);
+		$len = strlen($data);
+		$l = strlen($key);
+		$char = '';
+		for($i = 0; $i < $len; $i++){
+			if ($x == $l){
+				$x = 0;
+			}
+			$char .= substr($key, $x, 1);
+			$x++;
+		}
+		$str = '';
+		for($i = 0; $i < $len; $i++){
+			if(ord(substr($data, $i, 1)) < ord(substr($char, $i, 1))){
+				$str .= chr((ord(substr($data, $i, 1)) + 256) - ord(substr($char, $i, 1)));
+			}else{
+				$str .= chr(ord(substr($data, $i, 1)) - ord(substr($char, $i, 1)));
+			}
+		}
+
+		return $str;
+	}
+}
